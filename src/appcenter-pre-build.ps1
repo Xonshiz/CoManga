d3f9006f2e695bbb50dd2d81c5dad51f34ca4020
Xonshiz <kanojia24.10@gmail.com>
2019-09-09 11:57:37 -0400

    Updated PS Script

--- src/appcenter-pre-build.ps1
+++ src/appcenter-pre-build.ps1
@@ -3,3 +3,15 @@ $msiPath = "$($env:USERPROFILE)\AdMediator.msi"
 (New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/401703a0-263e-4949-8f0f-738305d6ef4b/file/146057/6/AdMediator.msi', $msiPath)
 cmd /c start /wait msiexec /i $msiPath /quiet
 Write-Host "Installed" -ForegroundColor green
+
+# Predefined Variables
+$adSdkUrl = "https://admediator.gallerycdn.vsassets.io/extensions/admediator/microsoftadvertisingsdk/10.1.00/1548989510766/MicrosoftAdvertisingSDK.msi"
+ $adSdkPath = Join-Path $env:TEMP "MicrosoftAdvertisingSDK.msi"
+ 
+# Download the files to local temp folder
+ Write-Output "downloading $adSdkUrl…"
+ Invoke-WebRequest -Uri $adSdkUrl -OutFile $adSDKPath
+ 
+# Install the SDKs (use the "qn" flag to install silently)
+ Write-Output "installing $adSdkPath…"
+ Start-Process $adSdkPath -ArgumentList "/q" -Wait
